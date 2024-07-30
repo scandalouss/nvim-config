a9d858d2385236d1b714737394b2f70833511a28
scandalouss <dking3211@gmail.com>
2024-07-30 12:03:23 -0500

    mess with lazy.nvim a bit

--- lua/david/lazyvim.lua
+++ lua/david/lazyvim.lua
@@ -16,8 +16,7 @@ require("lazy").setup({
 	--treesitter
 	{"nvim-treesitter/nvim-treesitter",build=":TSUpdate"},
 	--file explorer
-	"nvim-tree/nvim-tree.lua",
-	"nvim-tree/nvim-web-devicons",
+    {"nvim-tree/nvim-tree.lua", dependencies={"nvim-tree/nvim-web-devicons"}},
 	--lsp/mason
 	"neovim/nvim-lspconfig",
 	"williamboman/mason.nvim",
@@ -27,9 +26,7 @@ require("lazy").setup({
 	"hrsh7th/cmp-nvim-lsp",
 	"hrsh7th/nvim-cmp",
     --fuzzyfinder
-    "nvim-telescope/telescope.nvim",
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope-file-browser.nvim",
+    {"nvim-telescope/telescope.nvim", dependencies={"nvim-lua/plenary.nvim","nvim-telescope/telescope-file-browser.nvim"}},
     --splash screen
     "goolord/alpha-nvim",
     --bottom status bar
@@ -39,36 +36,33 @@ require("lazy").setup({
     --autopairs
     {"echasnovski/mini.pairs",version=false},
     --sidebar map
-    {"echasnovski/mini.map",version=false},
+    {"echasnovski/mini.map",version=false, lazy=true},
     --notifications
     {"echasnovski/mini.notify",version=false},
     --animations
     {"echasnovski/mini.animate",version=false},
     --highlight color patterns
-    {"echasnovski/mini.hipatterns",version=false},
+    {"echasnovski/mini.hipatterns",version=false, lazy=true},
     --status column
     "luukvbaal/statuscol.nvim",
     --folding lines/indent lines
-    "kevinhwang91/nvim-ufo",
-    "kevinhwang91/promise-async",
-    "lukas-reineke/indent-blankline.nvim",
+    {"kevinhwang91/nvim-ufo", lazy=true, dependencies={"kevinhwang91/promise-async"}},
+    {"lukas-reineke/indent-blankline.nvim", lazy=true},
     --diagnostics
-    "folke/trouble.nvim",
+    {"folke/trouble.nvim", lazy=true},
     --debugger
-    "mfussenegger/nvim-dap",
-    "rcarriga/nvim-dap-ui",
-    "nvim-neotest/nvim-nio",
+    {"rcarriga/nvim-dap-ui", lazy=true, dependencies={"mfussenegger/nvim-dap","nvim-neotest/nvim-nio"}},
     "jay-babu/mason-nvim-dap.nvim",
     --easy compiler
-    "stevearc/overseer.nvim",
+    {"stevearc/overseer.nvim", lazy=true},
     --project management
     "ahmedkhalf/project.nvim",
     --gentoo highlights
-    "gentoo/gentoo-syntax",
+    {"gentoo/gentoo-syntax", lazy=true},
     --colorcolumn to keep me at 80 lines
     "m4xshen/smartcolumn.nvim",
     --theprimeagen's vim games to make me better at this shit
-    "ThePrimeagen/vim-be-good",
+    {"ThePrimeagen/vim-be-good", lazy=true},
     --gruvbox theme for laptop
-    "ellisonleao/gruvbox.nvim",
+    {"ellisonleao/gruvbox.nvim", lazy=true},
 })
