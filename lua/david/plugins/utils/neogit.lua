f47f96475308bf534a17f913ffab109db70d39fa
scandalouss <dking3211@gmail.com>
2025-02-14 15:29:09 -0600

    boss wtf

--- lua/david/plugins/utils/neogit.lua
+++ lua/david/plugins/utils/neogit.lua
@@ -9,46 +9,6 @@ return{
         local neogit=require("neogit")
         neogit.setup{
             kind = "floating",
-            commit_editor = {
-               kind = "floating"
-            },
-            commit_select_view = {
-               kind = "floating"
-            },
-            commit_view = {
-                verify_commit = true,
-                kind = "floating"
-            },
-            log_view = {
-                kind = "floating",
-            },
-            rebase_editor = {
-                kind = "floating",
-            },
-            reflog_view = {
-                kind = "floating",
-            },
-            merge_editor = {
-                kind = "floating",
-            },
-            description_editor = {
-                kind = "floating",
-            },
-            tag_editor = {
-                kind = "floating",
-            },
-            preview_buffer = {
-                kind = "floating",
-            },
-            popup = {
-                kind = "floating",
-            },
-            stash = {
-                kind = "floating",
-            },
-            refs_view = {
-                kind = "floating",
-            },
             integrations = {
                 telescope = true
             }
